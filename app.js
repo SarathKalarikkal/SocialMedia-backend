d616ece4cc1e45fe0f09115af79487731659209b
Sarath K <114877155+SarathKalarikkal@users.noreply.github.com>
2024-03-15 18:56:15 +0530

    Update-static app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 
 app.use('/', require('./routers'))
 
-app.use('/public', express.static('public))
+app.use('/public', express.static('public'))
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
